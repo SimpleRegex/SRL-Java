ab7a759192c46290cacfed3c508face68ab02187
Marco <marcomoretti0103@gmail.com>
2016-09-01 22:42:53 +0200

    Add methods and ther super class Method

---
+++ src/main/java/language/methods/DefaultMethod.java
@@ -0,0 +1,11 @@
+package main.java.language.methods;
+
+import main.java.interfaces.Method;
+/**
+ * Created by marco on 31/08/16.
+ */
+public class DefaultMethod extends Method {
+    public DefaultMethod(String original, String methodName) {
+        super(original, methodName);
+    }
+}
---
+++ src/main/java/language/methods/ToMethod.java
@@ -0,0 +1,30 @@
+package main.java.language.methods;
+
+import main.java.exceptions.SyntaxException;
+import main.java.interfaces.Method;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by marco on 31/08/16.
+ */
+public class ToMethod extends Method {
+
+    public ToMethod(String original, String methodName) {
+        super(original, methodName);
+    }
+
+    public Method setParameters(List<Object> params) throws SyntaxException
+    {
+        params= Arrays.asList(Arrays.stream(params.toArray()).filter(item -> {
+            if(!(item instanceof String)){
+                return true;
+            }
+            String lower=((String)item).toLowerCase();
+            return !lower.equals("'to'") ;
+        }).toArray(Object[]::new));
+
+        return super.setParameters(params);
+    }
+}
