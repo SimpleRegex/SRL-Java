c6b7aeb840c20a71283cb2277fccfd4177ada39f
Marco <marcomoretti0103@gmail.com>
2016-09-01 22:20:23 +0200

    Add class builder. It must be developed

---
+++ src/main/java/Builder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java;
+
+/**
+ *
+ * @author marco
+ */
+public class Builder {
+    
+    public void group(Object o){
+        
+    }
+
+    public String get(char delimiter, boolean ignoreInvalid) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
