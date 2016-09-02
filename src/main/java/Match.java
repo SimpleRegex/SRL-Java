94b5f7c0537d7907ffe46f1a101d0156b412afd6
Samuel Tebbs <samuel.tebbs@yahoo.com>
2016-09-02 10:23:08 +0100

    Working on Match class

--- src/main/java/Match.java
+++ src/main/java/Match.java
@@ -1,14 +1,21 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by samuelt on 31/08/2016.
  */
 public class Match {
 
-    protected String[] rawData = {};
-    protected String[] attributes = {};
+    protected LinkedHashMap<Object, Object> rawData = new LinkedHashMap<>();
+    protected LinkedHashMap<String, Object> attributes = new LinkedHashMap<>();
 
-    public Match(String[] data) {
+    public Match(LinkedHashMap<Object, Object> data) {
         this.rawData = data;
-
+        rawData.entrySet().stream().filter(entry -> entry.getKey() instanceof String).forEach(entry -> attributes.put(entry.getKey().toString(), entry.getValue()));
+        if(attributes.isEmpty()) {
+            LinkedHashMap<Object, Object> copy = data.remove(data.ke)
+        }
     }
 
 }
