a08c246fb3f3e01a4ee74c4583e1e76ea42b3465
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-09-06 21:07:17 +0100

    Replaced analyse() with generate()

--- src/main/java/com/github/simpleregex/SRLParseTreeListener.java
+++ src/main/java/com/github/simpleregex/SRLParseTreeListener.java
@@ -1,9 +1,25 @@
 package com.github.simpleregex;
 
 import com.github.simpleregex.parser.SRLBaseListener;
+import com.github.simpleregex.parser.SRLParser;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 /**
  * Created by samtebbs on 06/09/2016.
  */
 public class SRLParseTreeListener extends SRLBaseListener {
+
+    StringBuffer result = new StringBuffer();
+
+    public String generate(SRLParser.QueryContext query) {
+        result = new StringBuffer();
+        ParseTreeWalker.DEFAULT.walk(this, query);
+        return result.toString();
+    }
+
+    @Override
+    public void enterQuery(SRLParser.QueryContext ctx) {
+        super.enterQuery(ctx);
+    }
+
 }
