fc0778472ef27e71a89368fd64ea4a7eb8f813e9
Marco <marcomoretti0103@gmail.com>
2016-09-08 17:33:05 +0200

    Add method to check if string matches reg expression, add some utils

--- src/main/java/com/github/simpleregex/SRL.java
+++ src/main/java/com/github/simpleregex/SRL.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -14,11 +18,28 @@ public class SRL {
 
     private final SRLCompiler compiler;
     private final Pattern pattern;
+    private final String regExpr;
+    private final String modifiers;
 
-    public SRL(SRLCompiler compiler) {
+    private SRL(SRLCompiler compiler) {
         this.compiler = compiler;
         this.compiler.parse();
-        this.pattern = Pattern.compile(this.compiler.generate());
+        this.regExpr=this.compiler.generate();
+        String[] regExpressionArray=regExpr.split("/");
+        this.modifiers=regExpressionArray[regExpressionArray.length-1];
+        this.pattern = Pattern.compile(String.join("", Arrays.copyOfRange(regExpressionArray,1,regExpressionArray.length-1)),getCompileModifiers());
+    }
+
+    private int getCompileModifiers(){
+        int compileModifier=0;
+        if(modifiers.contains("i")){
+            compileModifier|=Pattern.CASE_INSENSITIVE;
+        }
+        if(modifiers.contains("m")){
+            compileModifier|=Pattern.MULTILINE;
+        }
+
+        return compileModifier;
     }
 
     public SRL(String input) {
@@ -38,5 +59,23 @@ public class SRL {
         for (String line : Files.readAllLines(filePath)) sb.append(line).append("\n");
         return sb.toString();
     }
+
+    public boolean isMatching(String stringToMatch){
+        return getMatcher(stringToMatch).matches();
+    }
+
+    private Matcher getMatcher(String stringToMatch){
+        return this.pattern.matcher(stringToMatch);
+    }
+
+    public String getRegExpr(){
+        return this.regExpr;
+    }
+
+    public String getModifiers(){
+        return this.modifiers;
+    }
+
+
 
 }
