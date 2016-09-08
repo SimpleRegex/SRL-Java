4ff3f328da94540326cf285e81e82e75c7908d1e
Marco <marcomoretti0103@gmail.com>
2016-09-08 09:26:30 +0200

    Remove method to test becasuse it is not possible to test it now, add a new class to test the any of method

--- src/test/java/TestBeginWith.java
+++ src/test/java/TestBeginWith.java
@@ -18,6 +18,7 @@ public class TestBeginWith extends TestCase {
 
     /**
      * Test method begin with that accept one character
+     * Example: a,b,c
      */
     public void testBeginWithLetter(){
         SRLCompiler compiler = new SRLCompiler("begin with letter");
@@ -26,12 +27,37 @@ public class TestBeginWith extends TestCase {
     }
 
     /**
-     * Test method begin with that accept one of the character between quotes
+     * Test method begin with one letter or one digit
+     * Example: 1b
      */
-    public void testBeginWithSpecificCharacters(){
-        SRLCompiler compiler = new SRLCompiler("begin with '._%+-'");
+    public void testBeginWithOneLetterOrOneDigit(){
+        SRLCompiler compiler = new SRLCompiler("begin with (letter,digit)");
         compiler.parse();
-        assertEquals("/^[._%+-]/",compiler.generate());
+        assertEquals("/^(?:[a-z][0-9])/",compiler.generate());
+    }
+
+    /**
+     * Test method begin with letter and digit one or more
+     *
+     * Example: a1,b5h7
+     *
+     */
+    public void testBeginWithLetterDigitOneOrMore(){
+        SRLCompiler compiler = new SRLCompiler("begin with (letter,digit) once or more");
+        compiler.parse();
+        assertEquals("/^(?:[a-z][0-9])+/",compiler.generate());
+    }
+
+    /**
+     * Test method begin with one or more letter and one or more digit
+     *
+     * Example: a1,b5h7
+     *
+     */
+    public void testBeginWithOneOrMoreLetterAndOneOrMoreDigit(){
+        SRLCompiler compiler = new SRLCompiler("begin with any of(letter,digit) once or more");
+        compiler.parse();
+        assertEquals("/^(?:[a-z]|[0-9])+/",compiler.generate());
     }
 
 
