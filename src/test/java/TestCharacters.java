2f02c09d798f86cbeebe267ae186fe74478b3d4f
Samuel Tebbs <samuel.tebbs@yahoo.com>
2016-09-12 10:21:44 +0100

    Added more methods to TestCharacters

--- src/test/java/TestCharacters.java
+++ src/test/java/TestCharacters.java
@@ -1,9 +1,6 @@
 import com.github.simpleregex.error.SRLSyntaxError;
 import junit.framework.TestCase;
 
-/**
- * Created by samuelt on 08/09/2016.
- */
 public class TestCharacters extends TestCase {
 
     public void testLetter() throws SRLSyntaxError {
@@ -16,6 +13,34 @@ public class TestCharacters extends TestCase {
 
     public void testOneOf() throws SRLSyntaxError {
         assertEquals(Util.toRegex("one of \"abc\""), "/[abc]/");
+    }
+
+    public void testUppercaseLetter() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("uppercase letter"), "/[A-Z]/");
+    }
+
+    public void testAnyCharacter() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("any character"), "/\\w/");
+    }
+
+    public void testDigit() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("digit"), "/[0-9]/");
+    }
+
+    public void testAnything() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("anything"), "/./");
+    }
+
+    public void testNewLine() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("new line"), "/\\n/");
+    }
+
+    public void testTab() throws SRLSyntaxError {
+        assertEquals(Util.toRegex("tab"), "/\\t/");
+    }
+
+    public void testRaw() {
+        assertEquals(Util.toRegex("raw \"[a-zA-Z]\""), "/[a-zA-Z]/");
     }
 
 }
--- src/test/java/Util.java
+++ src/test/java/Util.java
@@ -1,9 +1,6 @@
 import com.github.simpleregex.SRLCompiler;
 import com.github.simpleregex.error.SRLSyntaxError;
 
-/**
- * Created by samuelt on 08/09/2016.
- */
 public class Util {
     public static String toRegex(String srlQuery) throws SRLSyntaxError {
         SRLCompiler compiler = new SRLCompiler(srlQuery);
